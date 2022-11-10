5a74a6fafd36bd6e85137b70d3b68c5c095d6731
rahul01r <rrdubeynibm@gmail.com>
2022-11-11 01:08:04 +0530

    [rahul] Add:UC4-MaxMethodMoreThenParametersUseSorting

---
+++ FindMaximumNumber/GenericsMaximum.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumNumber
+{
+    public class GenericsMaximum<T> where T : IComparable
+    {
+        public T[] value;
+        public GenericsMaximum(T[] value)
+        {
+            this.value = value;
+        }
+        public T[] Sort(T[] values)
+        {
+            Array.Sort(this.value, values);
+            return values;
+        }
+        public T MaxValue(params T[] values)
+        {
+            var sorted_values = Sort(values);
+            return sorted_values[^1];
+        }
+        public T MaxMethod()
+        {
+            var max = MaxValue(this.value);
+            return max;
+        }
+        public void PrintMaxValue()
+        {
+            var max = MaxValue(this.value);
+            Console.WriteLine("Maximum Value is " + max);
+        }
+
+
+
+    }
+}
+
+   
+
--- FindMaximumNumber/Program.cs
+++ FindMaximumNumber/Program.cs
@@ -4,60 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Text Maximum Number Problems Statement");
-            while (true)
-            {
-                Console.WriteLine("Select program \n1.Find The Greatest Number Among Three Integer Value\n2.Find The Greatest Number Among Three Float Value\n3.Find The Greatest Number Among Three String Value");
-                int option = Convert.ToInt32(Console.ReadLine());
-                switch (option)
-                {
-                    case 1:
-                        Console.WriteLine("Enter the First number: ");
-                        int first = Convert.ToInt32(Console.ReadLine());
+            int[] intArray = { 112, 344, 432, 555, 678 };
+            GenericsMaximum<int> genericsMaximum = new GenericsMaximum<int>(intArray);
+            genericsMaximum.PrintMaxValue();
+            double[] doubleArray = { 11.2, 32.5, 4.32, 55.5, 6.78 };
+            GenericsMaximum<double> genericdouble = new GenericsMaximum<double>(doubleArray);
+            genericdouble.PrintMaxValue();
+            string[] stringArray = { "ABC", "xyz", "def", "MNO", "axz" };
+            GenericsMaximum<string> genericstring = new GenericsMaximum<string>(stringArray);
+            genericstring.PrintMaxValue();
 
-                        Console.WriteLine("Enter the second number: ");
-                        int second = Convert.ToInt32(Console.ReadLine());
-
-                        Console.WriteLine("Enter the third number: ");
-                        int third = Convert.ToInt32(Console.ReadLine());
-
-                       MaximumNumberCheck<int> maxnumObj = new MaximumNumberCheck<int>(first, second, third);
-                        int maxNumber = maxnumObj.GetMaxNum();
-
-                        Console.WriteLine("your max number {0}", maxNumber);
-                        break;
-                    case 2:
-                        Console.WriteLine("Enter the First number: ");
-                        float firstf = Convert.ToSingle(Console.ReadLine());
-
-                        Console.WriteLine("Enter the second number: ");
-                        float secondf = Convert.ToSingle(Console.ReadLine());
-
-                        Console.WriteLine("Enter the third number: ");
-                        float thirdf = Convert.ToSingle(Console.ReadLine());
-
-                        MaximumNumberCheck<float> maxnumObj1 = new MaximumNumberCheck<float>(firstf, secondf, thirdf);
-                        var maxNumber1 = maxnumObj1.GetMaxNum();
-
-                        Console.WriteLine("your max float number {0}", maxNumber1);
-                        break ;
-                    case 3:
-                        Console.WriteLine("Enter the First string: ");
-                        string firsts = Console.ReadLine();
-
-                        Console.WriteLine("Enter the second string: ");
-                        string seconds = Console.ReadLine();
-
-                        Console.WriteLine("Enter the third string: ");
-                        string thirds = Console.ReadLine();
-
-                        MaximumNumberCheck<string> maxnumObj2 = new MaximumNumberCheck<string>(firsts, seconds, thirds);
-                        string last = maxnumObj2.GetMaxNum();
-
-                        Console.WriteLine("The last string in the sort order by comparedTo method is : {0}", last);
-                        break ;
-                }
-            }
         }
     }
+        
+    
 }
