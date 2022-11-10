f3d71f544adea63ce36cacfdebb0164e536796a0
rahul01r <rrdubeynibm@gmail.com>
2022-11-11 00:04:57 +0530

    [rahul] Add:UC3-FindMaximumValueAmongThreeStringValues

--- FindMaximumNumber/MaximumNumberCheck.cs
+++ FindMaximumNumber/MaximumNumberCheck.cs
@@ -32,7 +32,18 @@ namespace FindMaximumNumber
                 return thirdNum;
 
         }
+        public static string FindMaxStringNumber( string firstNum, string secondNum, String thirdNum)
+        {
+            if (firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) > 0)
+                return firstNum;
 
+            if (secondNum.CompareTo(firstNum) > 0 && secondNum.CompareTo(thirdNum) > 0)
+                return secondNum;
+
+            else
+                return thirdNum;
+
+        }
     }
 }
 
--- FindMaximumNumber/Program.cs
+++ FindMaximumNumber/Program.cs
@@ -7,7 +7,7 @@
             Console.WriteLine("Welcome to Text Maximum Number Problems Statement");
             while (true)
             {
-                Console.WriteLine("Select program \n1.Find The Greatest Number Among Three Integer Value\n2.Find The Greatest Number Among Three Float Value");
+                Console.WriteLine("Select program \n1.Find The Greatest Number Among Three Integer Value\n2.Find The Greatest Number Among Three Float Value\n3.Find The Greatest Number Among Three String Value");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -15,9 +15,13 @@
                         int Output = MaximumNumberCheck.FindMaxIntNumber(12, 11, 18);
                         Console.WriteLine("Greatest No. is:"+Output);
                         break;
-                        case 2:
+                    case 2:
                         double Output1 = MaximumNumberCheck.FindMaxDoubleNumber(12.5, 18.9, 5.5);
                         Console.WriteLine("Greatest No. is:"+Output1);
+                        break;
+                    case 3:
+                        string Output2 = MaximumNumberCheck.FindMaxStringNumber("abc", "MNO", "AbC");
+                        Console.WriteLine("Greatest No. is:"+Output2);
                         break;
                 }
             }
