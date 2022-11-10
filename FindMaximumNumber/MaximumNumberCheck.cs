cbbd13b36c8d4db1a4936a5c9597b7cb0c57645c
rahul01r <rrdubeynibm@gmail.com>
2022-11-10 23:41:18 +0530

    [rahul] Add:UC1-FindMaximumValueAmongThreeIntegersValues

---
+++ FindMaximumNumber/MaximumNumberCheck.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumNumber
+{
+    public class MaximumNumberCheck
+    {
+        public static int FindMaxIntNumber(int firstNum, int secondNum, int thirdNum)
+        {
+            if (firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) > 0)
+                return firstNum;
+                
+            if (secondNum.CompareTo(firstNum) > 0 && secondNum.CompareTo(thirdNum) > 0)
+                return secondNum;
+         
+            else
+                return thirdNum;
+                
+        }
+       
+    }
+}
+
+   
+
---
+++ FindMaximumNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace FindMaximumNumber
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Text Maximum Number Problems Statement");
+            while (true)
+            {
+                Console.WriteLine("Select program \n1.Find The Greatest Number Among Three Integer Value");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        int Output = MaximumNumberCheck.FindMaxIntNumber(12, 11, 18);
+                        Console.WriteLine("Greatest No. is:"+Output);
+                        break;
+
+                }
+            }
+        }
+    }
+}
